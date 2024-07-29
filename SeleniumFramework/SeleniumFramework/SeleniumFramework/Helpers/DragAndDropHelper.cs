ae7dcfd41a628e078b5438b14897fa9fdccae99f
Jerry Luther <jerry.luther@esg.global>
2024-07-29 11:40:28 -0600

    Added drag and drop methods

---
+++ SeleniumFramework/SeleniumFramework/SeleniumFramework/Helpers/DragAndDropHelper.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace SeleniumFramework.SeleniumFramework.Helpers
+{
+    public class DragAndDropHelper : HelpersBase
+    {
+        public void DragAndDropElement(IWebElement fromElement, IWebElement toElement, IWebDriver driver)
+        {
+            Actions builder = new Actions(driver);
+
+            IAction dragAndDrop = builder.ClickAndHold(fromElement).MoveToElement(toElement).Release(toElement).Build();
+
+            dragAndDrop.Perform();
+        }
+
+        public void DragAndDropWithLocator(By fromLocator, By toLocator, IWebDriver driver)
+        {
+            IWebElement fromElement = driver.FindElement(fromLocator);
+            IWebElement toElement = driver.FindElement(toLocator);
+            Actions builder = new Actions(driver);
+
+            IAction dragAndDrop = builder.ClickAndHold(fromElement).MoveToElement(toElement).Release(toElement).Build();
+
+            dragAndDrop.Perform();
+
+        }
+    }
+}
